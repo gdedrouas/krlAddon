91f569b4b64528482920581f4d21c3e9b60fc4a9
gdedrouas <geraud.dedrouas@free.fr>
2022-06-11 16:37:56 +0200

    Raise action bars

--- core.lua
+++ core.lua
@@ -27,3 +27,47 @@ f2:SetScript('OnEvent', function(self, event,...)
     end
 end)
 f2:RegisterEvent('GARRISON_SHOW_LANDING_PAGE')
+
+-- Raise actionbar and multibarbottomleft a little
+-- Scale the bars
+--for i = 1, 12 do
+--    _G["ActionButton"..i]:SetScale(1.04)
+--end
+--MultiBarBottomLeft:SetScale(1.04)
+--MultiBarBottomRight:SetScale(1.04)
+--wPetActionBarFrame:SetScale(1.04)
+-- Move ActionBar
+ActionButton1:ClearAllPoints()
+ActionButton1:SetPoint("CENTER",-230,120)
+ActionButton1.SetPoint = function() end
+-- Move MultiBarBottomLeft
+MultiBarBottomLeft:ClearAllPoints()
+MultiBarBottomLeft:SetPoint("LEFT", ActionButton1, "LEFT", 0, 45)
+MultiBarBottomLeft.SetPoint = function() end
+-- Move MutltiBarBottomRight and make it one bar
+MultiBarBottomRight:ClearAllPoints()
+MultiBarBottomRight:SetPoint("LEFT", MultiBarBottomLeft, "LEFT", 0, 45)
+MultiBarBottomRight.SetPoint = function() end
+MultiBarBottomRightButton1:ClearAllPoints() 
+MultiBarBottomRightButton1:SetPoint("LEFT", MultiBarBottomLeft, "LEFT", 0, 42)      
+MultiBarBottomRightButton1.SetPoint = function() end
+MultiBarBottomRightButton7:ClearAllPoints() 
+MultiBarBottomRightButton7:SetPoint("LEFT", MultiBarBottomRightButton6, "LEFT", 42, 0)      
+MultiBarBottomRightButton7.SetPoint = function() end    
+-- Move StanceBar
+StanceButton1:ClearAllPoints()
+StanceButton1:SetPoint("LEFT", MultiBarBottomLeft, "LEFT", 0, 42)
+StanceButton1.SetPoint = function() end
+-- Move PetActionBar
+PetActionBarFrame:ClearAllPoints()
+PetActionBarFrame:SetPoint("CENTER", MultiBarBottomLeft, "CENTER", 30, 90)
+PetActionBarFrame.SetPoint=function()end
+-- Move PageButtons
+ActionBarUpButton:ClearAllPoints()
+ActionBarUpButton:SetPoint("CENTER",650,-15)
+-- Hide Blizzard Art
+MainMenuBarArtFrame.LeftEndCap:Hide()
+MainMenuBarArtFrame.RightEndCap:Hide()
+MainMenuBarArtFrameBackground:Hide()
+MainMenuBarArtFrame.PageNumber:Hide()
+StatusTrackingBarManager:Hide()
