94a43e5a730196deac2765a0bb2154b295522be9
gdedrouas <geraud.dedrouas@free.fr>
2022-06-11 17:06:52 +0200

    Fix prep frames

--- core.lua
+++ core.lua
@@ -11,22 +11,31 @@ SetCVar('showArenaEnemyFrames', 0, 'SHOW_ARENA_ENEMY_FRAMES_TEXT')
 
 -- Hide arena prep frames
 -- Set up a dummy frame to listen for the event since the arena ui is not yet loaded
-local f1 = CreateFrame('Frame')
-f1:SetScript('OnEvent', function(self, event,...)
+local farenaprep = CreateFrame('Frame')
+farenaprep:SetScript('OnEvent', function(self, event,...)
     if event == 'ARENA_PREP_OPPONENT_SPECIALIZATIONS' then
       ArenaPrepFrames:Hide()
     end
 end)
-f1:RegisterEvent('ARENA_PREP_OPPONENT_SPECIALIZATIONS')
+farenaprep:RegisterEvent('ARENA_PREP_OPPONENT_SPECIALIZATIONS')
+
+local farenaprep_2 = CreateFrame('Frame')
+farenaprep_2:SetScript('OnEvent', function(self, event,...)
+    if event == 'ADDON_LOADED' and ... == 'Blizzard_ArenaUI' then
+      ArenaPrepFrames.Show = function() end
+    end
+end)
+farenaprep_2:RegisterEvent('ADDON_LOADED')
 
 --Hide Covenant minimap button
-local f2 = CreateFrame('Frame')
-f2:SetScript('OnEvent', function(self, event,...)
+local fcovbutton = CreateFrame('Frame')
+fcovbutton:SetScript('OnEvent', function(self, event,...)
     if event == 'GARRISON_SHOW_LANDING_PAGE' then
       GarrisonLandingPageMinimapButton:Hide()
     end
 end)
-f2:RegisterEvent('GARRISON_SHOW_LANDING_PAGE')
+fcovbutton:RegisterEvent('GARRISON_SHOW_LANDING_PAGE')
+
 
 -- Raise actionbar and multibarbottomleft a little
 -- Scale the bars
